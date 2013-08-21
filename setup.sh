7829ae65aebfe3e9c9a08436a589a0db1b15e249
Michael Nipper <mjn4406@gmail.com>
2013-08-21 15:22:50 -0400

    Add setup; prepare for placeholder dirs

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+rm -rf [FRAMEWORKBASE_PLACEHOLDER] [SETTINGS_PLACEHOLDER]
+
+git clone https://github.com/mnipper/platform_frameworks_base.git [FRAMEWORKBASE_PLACEHOLDER]
+
+git clone https://github.com/mnipper/platform_packages_apps_settings.git [SETTINGS_PLACEHOLDER]
