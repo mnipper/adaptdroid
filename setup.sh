ea91d6cd220e8b3d0871e253dadbe6de49100ca1
Michael Nipper <mjn4406@gmail.com>
2013-08-23 10:12:16 -0400

    Add in directories for build

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm -rf [FRAMEWORKBASE_PLACEHOLDER] [SETTINGS_PLACEHOLDER]
+rm -rf ~/WORKING_DIRECTORY/frameworks/base ~/WORKING_DIRECTORY/packages/apps/Settings
 
-git clone https://github.com/mnipper/platform_frameworks_base.git [FRAMEWORKBASE_PLACEHOLDER]
+git clone https://github.com/mnipper/platform/WORKING_DIRECTORY/frameworks/baseframeworks_base.git ~/WORKING_DIRECTORY/frameworks/base
 
-git clone https://github.com/mnipper/platform_packages_apps_settings.git [SETTINGS_PLACEHOLDER]
+git clone https://github.com/mnipper/platform/WORKING_DIRECTORY/frameworks/basepackages_apps_settings.git ~/WORKING_DIRECTORY/packages/apps/Settings
